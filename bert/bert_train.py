71639548d623d4d139f5dbc50cd1e33aaf033f25
ZhiyuanthePony <52745108+87003697@users.noreply.github.com>
2021-08-01 23:48:34 +0800

    Update bert_train.py

--- bert/bert_train.py
+++ bert/bert_train.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 23:31:06 2021
+
+@author: zhiyuan ma
+"""
 import torch
 import shutil, sys   
 import pdb
@@ -57,86 +64,86 @@ def train_model(start_epochs,  n_epochs, valid_loss_min_input,
                 training_loader, validation_loader, model, 
                 optimizer, checkpoint_path, best_model_path, device):
    
-  # initialize tracker for minimum validation loss
-  valid_loss_min = valid_loss_min_input 
- 
-  for epoch in range(start_epochs, n_epochs+1):
-    train_loss = 0
-    valid_loss = 0
-
-    model.train()
-    print('############# Epoch {}: Training Start   #############'.format(epoch))
-    for batch_idx, data in tqdm(enumerate(training_loader)):
-
-        # data.keys() = dict_keys(['summary_stemmed', 'pros_stemmed', 'cons_stemmed', 'targets'])
-        # data['targets'].keys() = dict_keys(['overall_ratings', 'work_balance_stars', 'culture_values_stars', 'carrer_opportunities_stars', 'comp_benefit_stars', 'senior_mangemnet_stars'])
-        # data['summary_stemmed'].keys() = dict_keys(['ids', 'mask', 'token_type_ids'])
-        # ...
-        data = set_input(data, device)
-        output = model(data)
-        # output.keys() = dict_keys(['overall_ratings', 'work_balance_stars', 'culture_values_stars', 'carrer_opportunities_stars', 'comp_benefit_stars', 'senior_mangemnet_stars'])
-        optimizer.zero_grad()
-        loss = loss_fn(output, data['targets'])
-        if batch_idx%5000==0:
-           print(f'Epoch: {epoch}, Training Loss:  {loss.item()}')
-        
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        #print('before loss data in training', loss.item(), train_loss)
-        train_loss = int(train_loss + ((1 / (batch_idx + 1)) * (loss- train_loss)))
-        #print('after loss data in training', loss.item(), train_loss)
+    # initialize tracker for minimum validation loss
+    valid_loss_min = valid_loss_min_input 
     
-    print('############# Epoch {}: Training End     #############'.format(epoch))
-    
-    print('############# Epoch {}: Validation Start   #############'.format(epoch))
-    ######################    
-    # validate the model #
-    ######################
- 
-    model.eval()
-   
-    with torch.no_grad():
-      for batch_idx, data in tqdm(enumerate(validation_loader, 0)):
+    for epoch in range(start_epochs, n_epochs+1):
+        train_loss = 0
+        valid_loss = 0
 
+        model.train()
+        print('############# Epoch {}: Training Start   #############'.format(epoch))
+        for batch_idx, data in tqdm(enumerate(training_loader)):
+
+            # data.keys() = dict_keys(['summary_stemmed', 'pros_stemmed', 'cons_stemmed', 'targets'])
+            # data['targets'].keys() = dict_keys(['overall_ratings', 'work_balance_stars', 'culture_values_stars', 'carrer_opportunities_stars', 'comp_benefit_stars', 'senior_mangemnet_stars'])
+            # data['summary_stemmed'].keys() = dict_keys(['ids', 'mask', 'token_type_ids'])
+            # ...
+            data = set_input(data, device)
             output = model(data)
-
-            loss = loss_fn(outputs, targets)
-            valid_loss = int(valid_loss + ((1 / (batch_idx + 1)) * (loss - valid_loss)))
-            # val_targets.extend(targets.cpu().detach().numpy().tolist()) 
-            # val_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
-
-      print('############# Epoch {}: Validation End     #############'.format(epoch))
-      # calculate average losses
-      #print('before cal avg train loss', train_loss)
-      train_loss = train_loss/len(training_loader)
-      valid_loss = valid_loss/len(validation_loader)
-      # print training/validation statistics 
-      print('Epoch: {} \tAvgerage Training Loss: {:.6f} \tAverage Validation Loss: {:.6f}'.format(
-            epoch, 
-            train_loss,
-            valid_loss
-            ))
-      
-      # create checkpoint variable and add important data
-      checkpoint = {
-            'epoch': epoch + 1,
-            'valid_loss_min': valid_loss,
-            'state_dict': model.state_dict(),
-            'optimizer': optimizer.state_dict()
-      }
+            # output.keys() = dict_keys(['overall_ratings', 'work_balance_stars', 'culture_values_stars', 'carrer_opportunities_stars', 'comp_benefit_stars', 'senior_mangemnet_stars'])
+            optimizer.zero_grad()
+            loss = loss_fn(output, data['targets'])
+            if batch_idx%5000==0:
+                print(f'Epoch: {epoch}, Training Loss:  {loss.item()}')
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            #print('before loss data in training', loss.item(), train_loss)
+            train_loss = int(train_loss + ((1 / (batch_idx + 1)) * (loss- train_loss)))
+            #print('after loss data in training', loss.item(), train_loss)
         
-        # save checkpoint
-      save_ckp(checkpoint, False, checkpoint_path, best_model_path)
+        print('############# Epoch {}: Training End     #############'.format(epoch))
         
-      ## TODO: save the model if validation loss has decreased
-      if valid_loss <= valid_loss_min:
-        print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_loss_min,valid_loss))
-        # save checkpoint as best model
-        # save_ckp(checkpoint, True, checkpoint_path, best_model_path)
-        valid_loss_min = valid_loss
+        print('############# Epoch {}: Validation Start   #############'.format(epoch))
+        ######################    
+        # validate the model #
+        ######################
+    
+        model.eval()
+    
+        with torch.no_grad():
+            for batch_idx, data in tqdm(enumerate(validation_loader, 0)):
 
-    print('############# Epoch {}  Done   #############\n'.format(epoch))
+                    data = set_input(data, device)
+                    output = model(data)
+                    loss = loss_fn(output, data['targets'])
+                    valid_loss = int(valid_loss + ((1 / (batch_idx + 1)) * (loss - valid_loss)))
+                    # val_targets.extend(targets.cpu().detach().numpy().tolist()) 
+                    # val_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
+
+        print('############# Epoch {}: Validation End     #############'.format(epoch))
+        # calculate average losses
+        #print('before cal avg train loss', train_loss)
+        train_loss = train_loss/len(training_loader)
+        valid_loss = valid_loss/len(validation_loader)
+        # print training/validation statistics 
+        print('Epoch: {} \tAvgerage Training Loss: {:.6f} \tAverage Validation Loss: {:.6f}'.format(
+                epoch, 
+                train_loss,
+                valid_loss
+                ))
+        
+        # create checkpoint variable and add important data
+        checkpoint = {
+                'epoch': epoch + 1,
+                'valid_loss_min': valid_loss,
+                'state_dict': model.state_dict(),
+                'optimizer': optimizer.state_dict()
+        }
+            
+            # save checkpoint
+        save_ckp(checkpoint, False, checkpoint_path, best_model_path)
+            
+        ## TODO: save the model if validation loss has decreased
+        if valid_loss <= valid_loss_min:
+            print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(valid_loss_min,valid_loss))
+            # save checkpoint as best model
+            save_ckp(checkpoint, True, checkpoint_path, best_model_path)
+            valid_loss_min = valid_loss
+
+        print('############# Epoch {}  Done   #############\n'.format(epoch))
 
     return model
 
