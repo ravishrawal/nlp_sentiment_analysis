3d2dc4b234af052837ee00e3024ae52a94c664f7
Ruiqi Li <rl3167@columbia.edu>
2021-08-01 11:49:34 -0400

    add data w/ senti and year

---
+++ FinalProject_Sentiment.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 18:54:34 2021
+
+@author: Ruiqi Li
+"""
+
+from utils import *
+
+base_path = "/Users/aprillee/Desktop/G5067 Natural Language Processing/Final Project/" 
+
+#Apply the function to get sentiment scores for summary, pros, and cons
+#review_data = open_pickle(base_path, "reviews_data_cleaned.pkl")
+#
+#review_data["sum_stem_sent"] = review_data.summary_stemmed.apply(score_sent)
+#
+#review_data["pros_stem_sent"] = review_data.pros_stemmed.apply(score_sent)
+#
+#review_data["cons_stem_sent"] = review_data.cons_stemmed.apply(score_sent)
+
+#review_data["advice_stem_sent"] = review_data.advice_to_mgmt_stemmed.apply(score_sent)
+
+#write_pickle(base_path, "review_with_sent_score.pkl", review_data)
+
+
+senti = open_pickle(base_path, "review_with_sent_score.pkl")
+with_date = open_pickle(base_path, "reviews_data_cleaned_w_dates.pkl")
+
+
+import pandas as pd
+from datetime import datetime
+with_date['year'] = pd.to_datetime(with_date['dates'], errors='coerce').dt.year
+
+data_with_year = pd.concat([senti, with_date['year']], axis=1)
+write_pickle(base_path, "review_w_sent_and_year.pkl", data_with_year)
